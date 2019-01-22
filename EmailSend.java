f07689cd225b2e694b7f977818d44cb6c8a4da9d
Lukalion <46938392+Lukalion@users.noreply.github.com>
2019-01-22 20:09:32 +0100

    Update EmailSend.java

--- EmailSend.java
+++ EmailSend.java
@@ -1,14 +1,4 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- * https://www.google.com/settings/security/lesssecureapps
- */
 
-
-/**
- *
- * @author Naveen
- */
 import java.util.*;
 import javax.mail.*;
 import javax.mail.internet.*;
@@ -17,12 +7,12 @@ public class EmailSend {
   private String host ="smtp.gmail.com" ;
     private String bestaetigungscode = "";
     private char[] rndm = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','0','1','2','3','4','5','6','7','8','9','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','0','1','2','3','4','5','6','7','8','9'};
-    private String user = "no.reply.reiseplaner@gmail.com";
-    private String pass = "Yaq12wsx";
+    private String user = "";
+    private String pass = "";
     private String to = "";
-    private String from = "no.reply.reiseplaner@gmail.com";
+    private String from = "";
     private String subject = "Thank you for your registration. Please enter your Code to activate your account.";
-    private String messageText = "<html><head><meta http-equiv=\"Content-Type\" content=\"text/html; charset=UTF-8\"><meta http-equiv=\"Content-Style-Type\" content=\"text/css\"><meta name=\"generator\" content=\"Aspose.Words for .NET 17.1.0.0\"><title></title></head><body><div><p style=\"margin-top:0pt; margin-bottom:10pt; line-height:115%; font-size:11pt\"><span style=\"font-family:Calibri\">Sehr geehrter Kunde,</span></p><p style=\"margin-top:0pt; margin-bottom:10pt; line-height:115%; font-size:11pt\"><span style=\"font-family:Calibri\">bitte geben sie folgenden Best‰tigungscode an:</span></p><p style=\"margin-top:0pt; margin-bottom:10pt; line-height:115%; font-size:28pt\"><span style=\"font-family:Calibri\">";
+    private String messageText = "<html><head><meta http-equiv=\"Content-Type\" content=\"text/html; charset=UTF-8\"><meta http-equiv=\"Content-Style-Type\" content=\"text/css\"><meta name=\"generator\" content=\"Aspose.Words for .NET 17.1.0.0\"><title></title></head><body><div><p style=\"margin-top:0pt; margin-bottom:10pt; line-height:115%; font-size:11pt\"><span style=\"font-family:Calibri\">Sehr geehrter Kunde,</span></p><p style=\"margin-top:0pt; margin-bottom:10pt; line-height:115%; font-size:11pt\"><span style=\"font-family:Calibri\">bitte geben sie folgenden Best√§tigungscode an:</span></p><p style=\"margin-top:0pt; margin-bottom:10pt; line-height:115%; font-size:28pt\"><span style=\"font-family:Calibri\">";
     private String messageText2 = "</span></p></div></body></html>";
     private Properties props = System.getProperties();
     private boolean sessionDebug = false;
